245b016ddd35dcc9a1c4b766158a4b66cd21502b
Christine Yu <christine.tc.yu@gmail.com>
2017-04-22 00:06:22 -0400

    CreateEvent mutation

--- meetdown/meetdown.py
+++ meetdown/meetdown.py
@@ -51,23 +51,23 @@ def initdb_command():
     init_db()
     print('Initialized the database.')
 
-
+# REST endpoints
 @app.route('/users', methods=['GET'])
 def get_users():
     db = get_db()
     cur = db.execute("""select
-            users.id as user_id,
-            username,
-            email,
-            GROUP_CONCAT(DISTINCT events.title) as events,
-            GROUP_CONCAT(groups.name) as groups
-            from users
-            LEFT OUTER JOIN memberships on memberships.user_id = users.id
-            LEFT OUTER JOIN groups on groups.id = memberships.group_id
-            LEFT OUTER JOIN signups on signups.user_id = users.id
-            LEFT OUTER JOIN events on events.id = signups.event_id
-            GROUP BY users.id, username, email
-            """);
+    users.id as user_id,
+    username,
+    email,
+    GROUP_CONCAT(DISTINCT events.title) as events,
+    GROUP_CONCAT(groups.name) as groups
+    from users
+    LEFT OUTER JOIN memberships on memberships.user_id = users.id
+    LEFT OUTER JOIN groups on groups.id = memberships.group_id
+    LEFT OUTER JOIN signups on signups.user_id = users.id
+    LEFT OUTER JOIN events on events.id = signups.event_id
+    GROUP BY users.id, username, email
+    """)
     users = [dict(u) for u in (cur.fetchall())]
     response = [{
         'user_id': u['user_id'],
@@ -82,7 +82,7 @@ def get_users():
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     db = get_db()
-    cur = db.execute('select id, username, email from users where id = ?', [user_id]);
+    cur = db.execute('select id, username, email from users where id = ?', [user_id])
     user = cur.fetchone()
     return jsonify(dict(user))
 
@@ -90,25 +90,45 @@ def get_user(user_id):
 @app.route('/groups', methods=['GET'])
 def get_groups():
     db = get_db()
-    cur = db.execute('select id, name from groups order by id desc');
+    cur = db.execute("""
+    SELECT groups.id, name, GROUP_CONCAT(users.username) as users
+    FROM groups
+    LEFT OUTER JOIN memberships on memberships.group_id = groups.id
+    INNER JOIN users on memberships.user_id = users.id
+    GROUP BY groups.id
+    """)
     rows = cur.fetchall()
-    return jsonify([dict(row) for row in rows])
+    response = [{
+        'group_id': r['id'],
+        'name': r['name'],
+        'users': list(set(r['users'].split(','))) if r['users'] else [],
+        } for r in rows]
+    return jsonify(response)
 
 
 @app.route('/groups/<int:group_id>', methods=['GET'])
 def get_group(group_id):
     db = get_db()
-    cur = db.execute('select id, name from groups where id = ?', [group_id]);
+    cur = db.execute("""
+    SELECT groups.id, name
+    GROUP_CONCAT(users.username) as users
+    FROM groups
+    LEFT OUTER JOIN memberships on memberships.group_id = groups.id
+    INNER JOIN memberships.user_id = users.id
+    WHERE id = ?
+    """, [group_id])
     row = cur.fetchone()
     return jsonify(dict(row))
 
 
-#class Query(graphene.ObjectType):
-    #hello = graphene.String()
+@app.route('/events', methods=['GET'])
+def get_events():
+    db = get_db()
+    cur = db.execute('select id, title, location from events order by id desc');
+    rows = cur.fetchall()
+    return jsonify([dict(row) for row in rows])
 
-    #def resolve_hello(self, args, context, info):
-        #return 'World'
-
+# graphql schema
 class Group(graphene.ObjectType):
     id = graphene.Int()
     name = graphene.String()
@@ -118,6 +138,7 @@ class Event(graphene.ObjectType):
     id = graphene.Int()
     title = graphene.String()
     location = graphene.String()
+    group_id = graphene.Int()
 
 
 class User(graphene.ObjectType):
@@ -153,15 +174,19 @@ class User(graphene.ObjectType):
 class Query(graphene.ObjectType):
     users = graphene.List(User, args=dict(id=graphene.ID()))
     groups = graphene.List(Group, args=dict(id=graphene.ID()))
+    events = graphene.List(Event, args=dict(id=graphene.ID()))
+    random = graphene.Int()
 
+    def resolve_random(self, args, context, info):
+        return 4
     def resolve_users(self, args, context, info):
         db = get_db()
         if 'id' not in args:
-            cur = db.execute("SELECT id, username, email from users");
+            cur = db.execute("SELECT id, username, email from users")
             users = [dict(u) for u in (cur.fetchall())]
             return [User(**u) for u in users]
 
-        cur = db.execute("SELECT id, username, email from users where users.id = ?", [args['id']]);
+        cur = db.execute("SELECT id, username, email from users where users.id = ?", [args['id']])
         users = [dict(u) for u in (cur.fetchall())]
         return [User(**u) for u in users]
 
@@ -175,6 +200,17 @@ class Query(graphene.ObjectType):
         cur = db.execute("SELECT id, name from groups where groups.id = ?", [args['id']]);
         users = [dict(u) for u in (cur.fetchall())]
         return [Group(**u) for u in users]
+
+    def resolve_events(self, args, context, info):
+        db = get_db()
+        if 'id' not in args:
+            cur = db.execute("SELECT id, title, location from events");
+            events = [dict(g) for g in (cur.fetchall())]
+            return [Event(**g) for g in events]
+
+        cur = db.execute("SELECT id, title, location from events where event.id = ?", [args['id']]);
+        events = [dict(u) for u in (cur.fetchall())]
+        return [Events(**u) for u in events]
 
 
 class CreateUser(graphene.Mutation):
@@ -195,8 +231,28 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user, ok=ok)
 
 
+class CreateEvent(graphene.Mutation):
+    class Input:
+        title = graphene.String()
+        location = graphene.String()
+        group_id = graphene.Int()
+
+    ok = graphene.Boolean()
+    event = graphene.Field(lambda: Event)
+
+    @staticmethod
+    def mutate(root, args, context, info):
+        event = Event(title=args.get('title'), location=args.get('location'), group_id=args.get('group_id'))
+        db = get_db()
+        cur = db.execute("INSERT into events (id, title, location, group_id) VALUES (null, ?, ?, ?)", [args.get('title'), args.get('location'), args.get('group_id')]),
+        db.commit()
+        ok = True
+        return CreateEvent(event=event, ok=ok)
+
+
 class MyMutations(graphene.ObjectType):
         create_user = CreateUser.Field()
+        create_event = CreateEvent.Field()
 
 schema = graphene.Schema(query=Query, mutation=MyMutations)
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
